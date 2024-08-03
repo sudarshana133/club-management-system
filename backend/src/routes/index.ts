bf7ff4f804f57c2a8fb57d57c5798ad8a583b071
darshanpuli <darshan.puli1166@gmail.com>
2024-08-03 15:08:34 +0530

    initialize

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,9 @@
+import express from "express"
+import { userRouter } from "./user";
+import { clubRouter } from "./club";
+import { eventRouter } from "./event";
+const router = express.Router();
+
+router.use('/user',userRouter);
+router.use('/club',clubRouter);
+router.use('/event',eventRouter);
