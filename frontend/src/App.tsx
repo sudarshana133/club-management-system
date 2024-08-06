52ee5b32a41ee0b9a36866266454daa3eb828860
ayush010123 <ayushc.is21@bmsce.ac.in>
2024-08-06 15:52:55 +0530

    sidebar navbar frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,23 @@
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Adminhome from "./pages/admin/Adminhome";
+
+const Signin = lazy(() => import("./pages/signin/Signin"));
+const SignUp = lazy(() => import("./pages/signup/Signup"));
+
 const App = () => {
   return (
-    <div className="text-red-500">App</div>
-  )
-}
-export default App
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<Adminhome />} />
+          
+        </Routes>
+      </Suspense>
+    </Router>
+  );
+};
+
+export default App;
